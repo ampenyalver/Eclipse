040507d33115731debe8d1841ae295c619427f12
Ana M Peñalver <ampenyalver@gmail.com>
2020-12-27 15:15:15 +0100

    segundo commit

---
+++ src/Almacen.java
@@ -0,0 +1,14 @@
+
+public class Almacen {
+	private double papas;
+    private double chocos;
+    
+    public Almacen(){
+        chocos=40;
+        papas=15;
+    };
+    public Almacen(double numPatatas, double numChocos){
+       papas=numPatatas;
+       chocos=numChocos;
+    }
+}
