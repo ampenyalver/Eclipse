b041a8c4353b3f68cb5e40e15db1e56ad94864b5
Ana M Peñalver <ampenyalver@gmail.com>
2020-12-27 15:05:29 +0100

    Primer commit

---
+++ egitEclipse/src/egitEclipse/Restaurante.java
@@ -0,0 +1,15 @@
+package egitEclipse;
+
+public class Restaurante {
+	private double papas;
+    private double chocos;
+
+    public Restaurante(){
+        chocos=40;
+        papas=15;
+    };
+    public Restaurante(double numPatatas, double numChocos){
+       papas=numPatatas;
+       chocos=numChocos;
+    }
+}
