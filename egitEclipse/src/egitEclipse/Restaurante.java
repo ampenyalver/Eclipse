96ee67bbf8509ec3d3af59576d8103632fcaa746
Ana M Peñalver <ampenyalver@gmail.com>
2020-12-27 15:21:15 +0100

    segundo commit

--- egitEclipse/src/egitEclipse/Restaurante.java
+++ egitEclipse/src/egitEclipse/Restaurante.java
@@ -12,4 +12,23 @@ public class Restaurante {
        papas=numPatatas;
        chocos=numChocos;
     }
+    public double addChocos(double masChocos){
+        return chocos=chocos+ masChocos;
+     }
+     public double addPapas( double masPapas){
+         
+        return papas=papas+ masPapas;
+     }
+     public void setChocos(double numChocos){
+         chocos=numChocos;
+     }
+     public double getChocos(){
+         return chocos;
+     }
+     public void setPapas(double numPatatas){
+        papas= numPatatas;
+     }
+     public double getPapas(){
+         return papas;
+     }
 }
